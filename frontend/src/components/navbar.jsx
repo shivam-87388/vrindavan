12d001267eb175aefd1849bf1809ca928e43d090
shivam-87388 <hardwellshivamsingh2002@gmail.com>
2026-09-19 23:01:48 +0530

    first commit

---
+++ frontend/src/components/navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const navbar = () => {
+  return (
+    <div className="flex flex-row items-center justify-between p-4">
+        <div className=" text-black text-base font-bold font-['Inter'] text-center">Vrindavan Home</div>
+    </div>
+  )
+}
+
+export default navbar;
