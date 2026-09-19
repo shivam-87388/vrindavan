dd32e861caf4904c11f8ac01d1f11bc45b50b807
shivam-87388 <hardwellshivamsingh2002@gmail.com>
2026-09-19 23:22:30 +0530

    add

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -5,11 +5,11 @@ const navbar = () => {
     <div className="flex flex-row items-center justify-between p-4">
         <div className=" text-black text-base font-bold font-['Inter'] text-center">Vrindavan Home</div>
         <div className=" flex justify-between">
-          <a href='#'>Home</a>
-          <a href='#'>Product</a>
-          <a href='#'>Categories</a>
-          <a href='#'>About</a>
-          <a href='#'>Contact</a>
+          <a href='#' className="text-slate-500 text-sm font-medium font-['Inter'] hover:text-blue-600">Home</a>
+          <a href='#' className="text-slate-500 text-sm font-medium font-['Inter'] hover:text-blue-600">Product</a>
+          <a href='#' className="text-slate-500 text-sm font-medium font-['Inter'] hover:text-blue-600">Categories</a>
+          <a href='#'className="text-slate-500 text-sm font-medium font-['Inter'] hover:text-blue-600">About</a>
+          <a href='#'className="text-slate-500 text-sm font-medium font-['Inter'] hover:text-blue-600">Contact</a>
         </div>
         <div> 
 
