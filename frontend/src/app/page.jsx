f90ac08b770afbfd14fd8a0585e19a84c2bc1bf2
shivam-87388 <hardwellshivamsingh2002@gmail.com>
2026-09-20 23:52:05 +0530

    add

--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const homepage = () => {
   return (
-    <div>
+    <div className="h-screen">
+      <div className="bg-[url('/hero.png')] bg-cover bg-center h-full">
+      <div className="flex flex-col items-center justify-start">
+        <h2 className="text-orange-500 text-sm font-semibold font-['Inter']">BUILDING BETTER TOMORROW</h2>
+
+      </div>
+      </div>
       
     </div>
   )
