83b241b3608a7b9143eacc8e232c341c9e12bf37
shivam-87388 <hardwellshivamsingh2002@gmail.com>
2026-09-21 01:10:48 +0530

    footer responsive

--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const homepage = () => {
   return (
     <div className="h-screen">
-      <div className="bg-[url('/hero.png')] bg-cover bg-center h-full">
+      <div className="bg-[url('/hero.png')] bg-cover bg-center min-h-[550px] w-full flex items-center px-10 md:px-20">
       <div className="flex flex-col items-center justify-start">
         <h2 className="text-orange-500 text-sm font-semibold font-['Inter']">BUILDING BETTER TOMORROW</h2>
 
--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -3,10 +3,10 @@ import { IconBrandFacebook, IconBrandWhatsapp, IconBrandInstagram, IconBrandLink
 
 const Footer = () => {
   return (
-    <div className="flex flex-col p-4 bg-slate-800 gap-2">
-      <div className="flex flex-row items-start">
+    <div className="flex flex-col pt-8 pb-4 px-8 bg-slate-800 gap-2">
+      <div className="flex  items-start lg:flex-nowrap flex-wrap gap-y-8">
         {/* column-1 */}
-        <div className="flex-1 flex-col item-start justify-start">
+        <div className="w-1/2 lg:flex-1 flex-col item-start lg:justify-start justify-around">
           <div className="text-white text-2xl font-medium font-['Poppins']">Vrindavan Home</div>
             <p className="text-white text-sm font-normal font-['Inter'] ">Your ine-stop shop for all construction material. Build your dream with us</p>
             <div className="flex items-center justify-start gap-3.5 mt-1.5">
@@ -17,9 +17,9 @@ const Footer = () => {
           </div>
         </div>
           {/* column-2 */}
-        <div className="flex-1 flex items-start justify-center gap-3">
+        <div className="w-1/2 lg:flex-1 flex items-start lg:justify-center justify-around gap-3">
         <h2 className="text-white text-base font-semibold font-['Poppins']">Quick Links
-        <ul className="text-white text-sm font-normal font-['Inter'] space-y-1.5 mt-1.5"> className="hover:cursor-pointer"
+        <ul className="text-white text-sm font-normal font-['Inter'] space-y-1.5 mt-1.5 lg:text-start text-center"> 
           <li className="hover:cursor-pointer">Home</li>
           <li className="hover:cursor-pointer">Product</li>
           <li className="hover:cursor-pointer">Categories</li>
@@ -28,22 +28,22 @@ const Footer = () => {
         </h2>
         </div>
         {/* column-3 */}
-       <div className="flex-1 flex items-start justify-center gap-3">
+       <div className="w-1/2 lg:flex-1 flex items-start lg:justify-center justify-start gap-3">
         <h2 className="text-white text-base font-semibold font-['Poppins']">Support
-        <ul className="text-white text-sm font-normal font-['Inter'] space-y-1.5 mt-1.5">
+        <ul className="text-white text-sm font-normal font-['Inter'] space-y-1.5 mt-1.5 lg:text-start text-center">
           <li className="hover:cursor-pointer">FAQ</li>
           <li className="hover:cursor-pointer">Shipping</li>
           <li className="hover:cursor-pointer">Help</li>
         </ul>
         </h2>
         </div>
-        {/* column- */}
-        <div className="flex-1 flex items-start justify-center">
+        {/* column-4 */}
+        <div className="w-1/2 lg:flex-1 flex items-start lg:justify-center justify-around">
           <div className="flex flex-col gap-1.5">
-           <h2 className="text-white text-base font-semibold font-['Poppins']">Newsletter</h2>
+           <h2 className="text-white text-base font-semibold font-['Poppins'] lg:text-start text-center">Newsletter</h2>
            <p className="text-white text-sm font-normal font-['Inter']">Subscribe to get update on new product and offers.</p>
-           <div className="flex flex-row bg-slate-700 w-fit gap-1.5 py-1.5 px-2 rounded-md focus-within:border-white focus-within:ring-1 focus-within:ring-white transition-all">
-        <input type="email" placeholder="email" className="focus:outline-none w-40 lg:w-48 text-white"></input>
+           <div className="flex flex-row bg-slate-700 w-fit gap-1.5 py-1.5 px-2 rounded-md focus-within:border-white focus-within:ring-1 focus-within:ring-white transition-all ">
+        <input type="email" placeholder="email" className="focus:outline-none w-full min-w-0 text-white"></input>
         <div className="bg-orange-600 rounded-md hover:cursor-pointer">
 
         <IconArrowNarrowRightDashed color='#ffffff' size={30} stroke={1.5}/>
@@ -55,9 +55,10 @@ const Footer = () => {
         </div>
 
       </div>
+      <hr className="border-slate-700 border-t-2"></hr>
       {/* copy right section */}
-        <div className="flex items-center justify-between">
-          <p className="text-white text-xs font-normal font-['Inter']">© 2026 Vrindavan Home. All rights reserved</p>
+        <div className="flex items-center justify-between gap-y-1 flex-wrap">
+          <p className="text-white text-xs font-normal font-['Inter'] lg:w-fit w-full lg:text-start text-center">© 2026 Vrindavan Home. All rights reserved</p>
           <p className="text-white text-xs font-normal font-['Inter'] hover:cursor-pointer">Developed by Shivam.dev</p>
           <p className="text-white text-xs font-normal font-['Inter']">Privacy Policy | Terms Conditions</p>
 
