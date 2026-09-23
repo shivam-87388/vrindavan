5df32bdb5174159881a27a00b75bf679345ea299
shivam-87388 <hardwellshivamsingh2002@gmail.com>
2026-09-23 13:35:31 +0530

    half part completer login page

--- frontend/src/app/login/page.jsx
+++ frontend/src/app/login/page.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import { IconMail , IconLock } from '@tabler/icons-react';
+import { IconMail , IconLock, IconArrowNarrowRight } from '@tabler/icons-react';
 
 const page = () => {
   return (
     <div className="min-h-screen flex items-center justify-center w-full bg-slate-50">
-      <form className="bg-white rounded-md shadow-md border border-bg-gray-200 px-2.5 py-3">
-        <div className="flex flex-col items-center justify-start w-full">
+      <form className="bg-white rounded-md shadow-md border border-bg-gray-200 px-4 py-3.5">
+        <div className="flex flex-col items-center justify-start w-full mb-6">
            <h1 className="w-full text-black text-3xl font-bold font-['Poppins']">Admin Login</h1>
            <p className="w-full text-black text-base font-normal font-['Inter']">Sign in to access your admin panel</p>
         </div>
@@ -13,7 +13,7 @@ const page = () => {
         <div className="w-full flex flex-col items-center justify-start gap-2.5">
         {/* email section */}
         <div className="w-full flex flex-col items-center justify-start">
-         <label htmlFor="user-email" class="w-full text-black text-sm font-bold font-['Inter']">Email Address</label>
+         <label htmlFor="user-email" className="w-full text-black text-sm font-bold font-['Inter']">Email Address</label>
          {/* icon + input field */}
         <div className=" flex-row flex w-full items-center justify-center gap-0.5 border border-gray-200 rounded-md">
           <IconMail/>
@@ -21,18 +21,29 @@ const page = () => {
         </div>
         {/* password section */}
          <div className="w-full flex flex-col items-center justify-start">
-          <label htmlFor="user-password" class="w-full text-black text-sm font-bold font-['Inter']">Password</label>
+          <label htmlFor="user-password" className="w-full text-black text-sm font-bold font-['Inter']">Password</label>
            {/* icon + input field */}
           <div className=" flex-row flex w-full items-center justify-center gap-0.5 border border-gray-200 rounded-md">
             <IconLock/>
             <input type="password" id="user-password" placeholder="Enter your password" className="w-full flex items-center"></input>
           </div>
-          
+          <a href="#" className="w-full flex justify-end text-sm text-orange-600 font-medium font-['Inter']">forget password?</a>
+         </div>
+         {/* login button */}
+         <div className="flex w-full flex-col items-center justify-center">
+          <div className=" flex-row w-full flex items-center justify-center gap-1 bg-orange-600 px-3.5 py-2.5 rounded-md hover:cursor-pointer hover:bg-orange-700">
+            <button className="flex justify-center  text-white text-base font-semibold font-['Inter']">login</button>
+            <IconArrowNarrowRight stroke={2} color="#ffffff" />
+          </div>
+          <hr className="w-full border-t-2 mt-5 mb-2"></hr>
+          <div className="flex items-center justify-start gap-1">
+            <IconLock stroke={2} color="#ED6200"/>
+            <p className="text-black text-base font-normal font-['Inter']">only authorized  personnel can access this panel</p>
+          </div>
          </div>
 
         </div>
         
-        {/* password section */}
         
         
         
