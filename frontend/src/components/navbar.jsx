343f9f82f3b20cb2ee03317e69e011586fd7688c
shivam-87388 <hardwellshivamsingh2002@gmail.com>
2026-09-25 21:04:05 +0530

    fix nav bar

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -5,29 +5,18 @@ import { IconShoppingCart, IconSearch } from '@tabler/icons-react';
 
 const navbar = () => {
   return (
-   <div className="flex flex-row w-full items-center px-40 pb-0.5  pt-7">
-    <h3 className="flex-1 text-black text-lg font-semibold font-['Poppins']">Vrindavan<br/>Home</h3>
-    <nav className="flex-1 flex flex-row justify-center gap-4">
-     <a href="text-slate-500 text-sm font- font-['Inter']">Home</a>
-     <a href="text-slate-500 text-sm font-bold font-['Inter']">Product</a>
-     <a href="text-slate-500 text-sm font-bold font-['Inter']">Categories</a>
-     <a href="text-slate-500 text-sm font-bold font-['Inter']">About</a>
-     <a href="text-slate-500 text-sm font-bold font-['Inter']">Contact</a>
+    
+  <header className="flex flex-row pt-7 items-center justify-between px-40">
+    <div className="flex-1 flex items-center justify-start">
+      <h3 className="text-black text-lg font-semibold font-['Poppins']">Vrindavan Home</h3>
+    </div>
+
+    <nav className="flex-1 flex justify-center">
+      <a href=''className="text-slate-500 text-sm font-bold font-['Inter']">Home</a>
     </nav>
-    <div className="flex items-center rounded-md border-2 bg-slate-100 border-zinc-700 py-1 px-1.5">
-      <input type="text" placeholder="Search for products..." className="text-zinc-700 text-base font-normal font-['Inter']"></input>
-      <IconSearch color='#2068F4' stroke={2} size={40} className="border-l-3 border-zinc-700 rounded-md px-0.5"/>
+    <div className="flex-1 flex justify-end">dc</div>
+  </header>
 
-    </div>
-    <div className="flex items-center justify-center gap-2">
-      <a href='#' className="text-blue-600 text-base font-semibold font-['Inter'] w-24 px-4 py-1.25">Login</a>
-    </div>
-    
-   
-   
-    
-
-   </div>
   )
 }
 
