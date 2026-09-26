f6d1df8db38b453416f75aba813638e7daaa2154
shivam-87388 <hardwellshivamsingh2002@gmail.com>
2026-09-26 13:27:08 +0530

    fix

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -1,12 +1,30 @@
 'use client'
-import React from 'react'
-import { IconShoppingCart, IconSearch } from '@tabler/icons-react';
+import React, { useState, useEffect, useRef } from "react";
+import { IconShoppingCart, IconSearch, IconArrowsCross,IconMenu2 } from '@tabler/icons-react';
 
 
 const navbar = () => {
+  const [show, setShow] = useState(false);
+  const navRef = useRef(null);
+  const handleClick = () => {
+    setShow(!show);
+  };
+    useEffect(() => {
+    const handleOutsideClick = (event) => {
+    if (navRef.current && !navRef.current.contains(event.target)) {
+      setShow(false);
+       }
+    };
+
+  document.addEventListener("mousedown", handleOutsideClick);
+  return () => {
+    document.removeEventListener("mousedown", handleOutsideClick);
+  };
+}, []);
   return (
     <div>
-      <header className="block md:flex flex-row pt-7 pb-4 items-center justify-between px-4 ">
+      {/* desktop navbar */}
+      <header className="hidden md:flex flex-row pt-7 pb-4 items-center justify-between px-4 ">
     <div className="flex-1 flex items-center justify-start">
       <h3 className="text-black text-lg font-semibold font-['Poppins']">Vrindavan Home</h3>
     </div>
@@ -32,6 +50,28 @@ const navbar = () => {
       
     </div>
   </header>
+
+{/* mobile navbar */}
+  <header className="relative flex md:hidden items-center justify-between p-4">
+        <div className="flex justify-start">
+       <h3 className="text-black text-lg font-semibold font-['Poppins']">Vrindavan Home</h3>
+        </div>
+
+         <div ref={navRef} className="flex flex-col mr-2.5">
+          <button onClick={handleClick} className="px-2.5 py-1.5">{show ?  <IconArrowsCross stroke={2} color="#050505" />: <IconMenu2 stroke={2} color="#050505" />}</button>
+        {show? <nav className="absolute right-0 top-full mt-2 flex flex-col items-center justify-center px-2.5 py-1.5 gap-1.5 rounded-md border-2 border-black bg-white shadow-lg z-50 w-36">
+          
+          <a href='#'className="flex item-center justify-center text-slate-500 text-sm font-bold font-['Inter'] hover:text-white hover:bg-blue-600 w-full py-1.5 px-0.5 rounded-md">Home</a>
+          <a href='#'className="flex item-center justify-center text-slate-500 text-sm font-bold font-['Inter'] hover:text-white hover:bg-blue-600 w-full py-1.5 px-0.5 rounded-md">Product</a>
+          <a href='#'className="flex item-center justify-center text-slate-500 text-sm font-bold font-['Inter'] hover:text-white hover:bg-blue-600 w-full py-1.5 px-0.5 rounded-md">Categories</a>
+          <a href='#'className="flex item-center justify-center text-slate-500 text-sm font-bold font-['Inter'] hover:text-white hover:bg-blue-600 w-full py-1.5 px-0.5 rounded-md">About</a>
+          <a href='#'className="flex item-center justify-center text-slate-500 text-sm font-bold font-['Inter'] hover:text-white hover:bg-blue-600 w-full py-1.5 px-0.5 rounded-md">Contact</a>
+        </nav>:""}
+
+         </div>
+        
+        
+    </header>
 
 
   
